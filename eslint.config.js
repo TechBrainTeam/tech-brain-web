e1b446101f1041b73d8f24c64199c234e389dabe
Alaiddin Bilginer <alaiddinbilginer@outlook.com>
2025-07-29 11:36:42 +0300

    chore: initial project structure

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['node_modules', 'dist'],
+  },
+  {
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+  prettier,
+];
