c5dfd8f62f21360a7c8dc557089bf217b20c7064
Alaiddin Bilginer <alaiddinbilginer@outlook.com>
2025-07-30 22:35:30 +0300

    feat: home domain

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from './App';
 import { RegisterPage } from '../domains/auth/pages/RegisterPage';
 import LoginPage from '../domains/auth/pages/LoginPage';
+import HomePage from '../domains/home/pages/HomePage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <HomePage />,
   },
   {
     path: '/register',
